7cb5545a7329a1a064682b4d6ad115f5c1e9417c
juhoi.kim <juhoi.kim@gmail.com>
2020-04-22 00:23:11 +0900

    ~ page 159

--- my_to_do_app/views.py
+++ my_to_do_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
 def index(request):
     return render(request, 'my_to_do_app/index.html')
+
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content=user_input_str)
+    new_todo.save()
+    return HttpResponse("create Todo~! 가즈아~! => " + user_input_str)
